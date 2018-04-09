987e9e3d3cd064da247d9a6323dcfdc699b7cf2c
grossyoan <32864409+grossyoan@users.noreply.github.com>
2018-04-09 11:48:37 +0200

    Début de JS
    Add du premier menu du jeu

---
+++ scripts/homepage.js
@@ -0,0 +1,9 @@
+$("#menuColor1").click(function(){
+  $('#menuColor2').css('display', 'none');
+  $('#menuColor1').css('width', '100%');
+});
+$("#menuColor2").click(function(){
+  $('#menuColor1').css('display', 'none');
+  $('#menuColor2').css('width', '100%');
+
+});
